34d69358f8e0826b9e0b551a9db9fb382c6f2b51
priyanga mary savarinathan <priyangapriyanga9025@gmail.com>
2026-06-23 21:29:26 +0530

    Add files via upload

---
+++ thread.java
@@ -0,0 +1,15 @@
+
+    class MyThread extends Thread {
+ public void run() {
+ System.out.println(
+ "Thread Running");
+ }
+}
+public class thread {
+ public static void main(String[] args) {
+ MyThread t =
+ new MyThread();
+ t.start();
+ }
+}
+
